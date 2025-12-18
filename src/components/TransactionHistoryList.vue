cc55ff3cf12f9974ac730d82a2acec610df00276
Inna <dechevaiv@gmail.com>
2025-12-18 14:28:07 +1100

    Implement component: TransactionHistoryList

---
+++ src/components/TransactionHistoryList.vue
@@ -0,0 +1,30 @@
+<script>
+import TransactionHistoryItem from './TransactionHistoryItem.vue'
+export default {
+  components: { TransactionHistoryItem },
+
+  props: ['transactionHistory'],
+
+  emit: ['history-deleted'],
+
+  methods: {
+    deleteTransaction(id) {
+      this.$emit(
+        'history-deleted',
+        this.transactionHistory.filter(t => t.id !== id)
+      )
+    },
+  },
+}
+</script>
+
+<template>
+  <ul>
+    <IncomeCategoryItem
+      v-for="transaction of transactionHistory"
+      v-bind:key="transaction.id"
+      v-bind:transaction="transaction"
+      v-on:transaction-deleted="deleteTransaction($event)"
+    />
+  </ul>
+</template>
