d5365831db482b3c0c5c19fabda3c1f34df14da6
Inna <dechevaiv@gmail.com>
2025-12-18 14:54:28 +1100

    Implemented component:OutcomeCategoryItem

---
+++ src/components/OutcomeCategoryItem.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: ['category'],
+
+  emits: ['outcomecategory-deleted'],
+}
+</script>
+
+<template>
+  <li class="m-3">
+    <span>{{ category.caption }}</span>
+    <button
+      type="button"
+      class="btn btn-secondary btn-sm ms-3"
+      v-on:click="$emit('outcomecategory-deleted', category.id)"
+    >
+      Delete
+    </button>
+  </li>
+</template>
